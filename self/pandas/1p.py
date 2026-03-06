f16994807c3bddcfce18f6e87b6a8399cabc36e5
Muhammad-Arsalan-Khan <arsalan.debug@gmail.com>
2026-03-07 04:11:07 +0500

    pandas

---
+++ self/pandas/1p.py
@@ -0,0 +1,23 @@
+#Series:
+# A Series is a one-dimensional labeled array that can hold any data type: integers, floats, strings, or even Python
+# objects. Each element in the Series has a unique label called an index.
+# It is often used to track changes or patterns over time, such as daily temperatures, stock prices, or sales revenue.
+
+# DataFrame:
+# A DataFrame is a two-dimensional labeled data structure in Pandas, similar to a table in a database, an Excel
+# spreadsheet, or a SQL table.
+# It consists of rows and columns, where:
+# Rows have indices (labels).
+# Columns have names (labels).
+
+# humm file ko read karty waqt uncoding lagaye gay.
+# agar large file hain to usko read karne ke liye chunk size use kar sakty hain.
+# or agar koi data cloude se read karna hain tu os ky python library use kar sakty hain jese gcsfs.
+
+# offical sdk use kar sakty hain.
+# boto3 for AWS S3, google-cloud-storage for Google Cloud Storage, or azure-storage-blob for Azure Blob Storag
+
+# Data               ---> Scientist	gcsfs
+# ML Engineer	     ---> gcsfs + SDK dono
+# Backend Developer  ---> Official SDK
+# DevOps	         ---> Official SDK
